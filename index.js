832db09e5a5a5e6a8c59a8ddaa5b08b5b034246c
Avijit Roy <royavijit782@gmail.com>
2022-03-23 05:11:16 +0600

    city corporation specific user load done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const admin = require("firebase-admin");
 const cors = require('cors');
 require('dotenv').config();
 const serviceAccount = require('./smart-waste-management-firebase-adminsdk.json');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,6 +18,7 @@ admin.initializeApp({
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wipcb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -46,10 +48,43 @@ async function run() {
 
         //save to city corporation users database collection
         app.post('/cityCorporationUsers', async (req, res) => {
-            const cityCorpUserInfos = req.body;
-            const result = await cityCorporationUsers.insertOne(cityCorpUserInfos);
-            res.json(result);
+            const email = req.body.email;
+            const query = { email: email };
+            const existingUser = await cityCorporationUsers.findOne(query);
+            if (existingUser) {
+                res.json({ message: 'Admin User is already exist' })
+            }
+            else {
+                const displayName = req.body.name;
+                const pic = req.files.photo;
+                const picData = pic.data;
+                const enCodedPic = picData.toString('base64');
+                const photoBuffer = Buffer.from(enCodedPic, 'base64');
+                const cityCorpUserInfos = {
+                    displayName,
+                    email,
+                    photo: photoBuffer
+                }
+                const result = await cityCorporationUsers.insertOne(cityCorpUserInfos);
+                res.json(result);
+            }
         });
+
+        //load specific city corporation user info by id from database collection for navbar
+        app.get('/cityCorporationUsers/searchByEmail/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const specificCityCorpUser = await cityCorporationUsers.findOne(query);
+            res.send(specificCityCorpUser);
+        })
+
+        //load specific city corporation user info by id from database collection for navbar
+        app.get('/cityCorporationUsers/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const specificCityCorpUser = await cityCorporationUsers.findOne(query);
+            res.send(specificCityCorpUser);
+        })
 
         //load city corporation users from database collection
         app.get('/cityCorporationUsers', verifyToken, async (req, res) => {
@@ -59,28 +94,51 @@ async function run() {
             res.send(cityCorpUsers);
         })
 
+
         //save to truck drivers users database collection
         app.post('/truckDriverUsers', async (req, res) => {
-            const truckDriverInfos = req.body;
-            const query = { nid: truckDriverInfos.nid };
+            const nid = req.body.nid;
+            const query = { nid: nid };
             const existingUser = await truckDriverUsers.findOne(query);
             if (existingUser) {
                 res.json({ message: 'User is already exist' })
             }
             else {
+                const displayName = req.body.name;
+                const age = req.body.age;
+                const gender = req.body.gender;
+                const phone = req.body.phone;
+                const pin = req.body.pin;
+                const dln = req.body.dln;
+                const address = req.body.address;
+                const pic = req.files.photo;
+                const picData = pic.data;
+                const enCodedPic = picData.toString('base64');
+                const photoBuffer = Buffer.from(enCodedPic, 'base64');
+                const truckDriverInfos = {
+                    displayName,
+                    age,
+                    gender,
+                    phone,
+                    pin,
+                    nid,
+                    dln,
+                    address,
+                    photo: photoBuffer
+                }
                 const result = await truckDriverUsers.insertOne(truckDriverInfos);
-                console.log(result);
                 res.json(result);
             }
         });
 
-        //load truck driver users from database collection
-        app.get('/truckDriverUsers', async (req, res) => {
-            const cursor = truckDriverUsers.find({});
-            const truckDrivers = await cursor.toArray();
-            res.send(truckDrivers);
+        //load specific truck driver info by nid from database collection for login for truck driver
+        app.get('/truckDriverUsers/details/:nid', async (req, res) => {
+            const nid = req.params.nid;
+            const query = { nid: nid };
+            const specificTruckDriver = await truckDriverUsers.findOne(query);
+            res.send(specificTruckDriver);
         })
-
+        
         //load specific truck driver info by id from database collection for navbar
         app.get('/truckDriverUsers/:id', async (req, res) => {
             const id = req.params.id;
@@ -89,12 +147,11 @@ async function run() {
             res.send(specificTruckDriver);
         })
 
-        //load specific truck driver info by nid from database collection for login for truck driver
-        app.get('/truckDriverUsers/details/:nid', async (req, res) => {
-            const nid = req.params.nid;
-            const query = { nid: nid };
-            const specificTruckDriver = await truckDriverUsers.findOne(query);
-            res.send(specificTruckDriver);
+        //load all truck driver users from database collection
+        app.get('/truckDriverUsers', async (req, res) => {
+            const cursor = truckDriverUsers.find({});
+            const truckDrivers = await cursor.toArray();
+            res.send(truckDrivers);
         })
 
     } finally {
